3f67e0167625af5d86e47c4981989928f754497d
Shrikant Vishwakarma <zeeroiq@gmail.com>
2019-09-06 22:22:02 +0530

    Movie info service

---
+++ movie-rate-service/src/main/java/com/shri/movierateservice/MovieRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shri.movierateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRateServiceApplication.class, args);
+    }
+
+}
---
+++ movie-rate-service/src/test/java/com/shri/movierateservice/MovieRateServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shri.movierateservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieRateServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
