8ad1ef9b6b555a0124be866c0187e85400c665c0
Shrikant Vishwakarma <zeeroiq@gmail.com>
2019-09-08 01:17:03 +0530

    Added Hystrix Dashboard support, which ease to monitor micro services

--- movie-catalog-service/src/main/java/com/shri/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/shri/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient // not needed it is done automatically spring cloud's eureka-client dependency which is the default
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
     @Bean
