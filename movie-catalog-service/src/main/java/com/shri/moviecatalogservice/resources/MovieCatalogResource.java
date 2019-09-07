ecc652f7b5d321fdd900ce4187b66196e8d775ba
Shrikant Vishwakarma <zeeroiq@gmail.com>
2019-09-07 12:12:52 +0530

    WebClient implementation of the REST call (ASYNCHRONOUS way)

--- movie-catalog-service/src/main/java/com/shri/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/shri/moviecatalogservice/resources/MovieCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,16 +26,26 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String item) {
-        // 1. get all rated movies ID
+    // 1. get all rated movies ID
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234", 3),
                 new Rating("321", 5)
         );
 
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = webClientBuilder.build() // builder pattern will give you a client
+                    .get() // tells i'm going to do a get call
+                    .uri("http://localhost:8081/movies/" + rating.getMovieId()) // specifies the uri
+                    .retrieve() // tells to spring to go to the fetch
+                    .bodyToMono(Movie.class) // tells whatever you get into the body part convert it into the instance of Movie class. here mono is kind of a promise that in future you are going to get this/use this which is an ASYNCHRONOUS call
+                    .block();// since initially mono will return a empty body and we have promised to return a list of CatalogItems we need to wait until something is there in the body. this thing is done here by the block() call
+
             return new CatalogItem(movie.getName(), "Successor of Infinity War", rating.getRating());
             }).collect(Collectors.toList());
 
